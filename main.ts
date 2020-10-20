9e41c87aeb3b7d9285d025ffbd9e54521f1c3578
410934047 <71689110+410934047@users.noreply.github.com>
2020-10-20 14:15:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+let list: number[] = []
+basic.forever(function () {
+    list = [4, 3, 2, 1, 0]
+    for (let value of list) {
+        led.plot(value, 4)
+        basic.pause(100)
+    }
+    list.reverse()
+    for (let value of list) {
+        led.plot(value, 3)
+        basic.pause(100)
+    }
+    list.reverse()
+    for (let value of list) {
+        led.plot(value, 2)
+        basic.pause(100)
+    }
+    list.reverse()
+    for (let value of list) {
+        led.plot(value, 1)
+        basic.pause(100)
+    }
+    list.reverse()
+    for (let value of list) {
+        led.plot(value, 0)
+        basic.pause(100)
+    }
+})
